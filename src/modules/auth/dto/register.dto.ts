8f762987b8eaeccb7adb51f8a0e7c24ff77e342c
bowfaro <giabao5443@gmail.com>
2026-07-24 08:48:51 +0700

    swagger

--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,13 +1,20 @@
 import { IsNotEmpty, IsPhoneNumber } from '@nestjs/class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({
+    description: '+84xxxxxxxxx',
+  })
   @IsNotEmpty()
   @IsPhoneNumber('VN', { message: 'Phone number is not valid' })
   phone: string;
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
+  @ApiProperty()
   @IsNotEmpty()
   email: string;
 }
--- src/modules/auth/dto/verify-otp.dto.ts
+++ src/modules/auth/dto/verify-otp.dto.ts
@@ -1,9 +1,10 @@
 import { IsNotEmpty, IsPhoneNumber } from '@nestjs/class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class VerifyOtpDto {
   @IsNotEmpty()
   @IsPhoneNumber('VN', { message: 'Phone number is not valid' })
   phone: string;
+  @ApiProperty()
   @IsNotEmpty()
   otp: string;
 }
